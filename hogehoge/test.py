272b4ab5f0ca95a0f7412d8a441638680fd75371
feather417417417 <109892530+feather417417417@users.noreply.github.com>
2023-02-07 21:00:17 +0900

    ピラミッド作成中 その2 基盤ができかけている！

--- hogehoge/test.py
+++ hogehoge/test.py
@@ -1,12 +1,15 @@
 from turtle import *
 shape("turtle")
 n = int(input('ピラミッドの段数を入力してください。'))
-for i in range(n):
+if n == 1:
+ for n in range(3):
+    left(120)
     forward(100)
-    left(45)
-    forward(100)
-    left(45)
-    forward(100)
-    left(45)
+elif n == 2:
+    left(120)
+    forward(50)
+    left(120)
+    forward(50)
+    left(120)
     forward(100)
 done()
