24c186e14e1f4170153e7db3f80f100b91074b6f
feather417417417 <109892530+feather417417417@users.noreply.github.com>
2023-02-08 20:57:24 +0900

    二段目まで完成！とメモ置き場作成

---
+++ hogehoge/memo.py
@@ -0,0 +1,17 @@
+#3段目増やしたいけどエラーにつきメモ
+elif n == 3:
+ for n in range(3):E
+    forward(50)
+    left(120)
+    continue
+right(120)
+for n in range(3):
+    forward(50)
+    left(120)
+    continue
+forward(50)
+left(120)
+forward(50)
+for n in range(3):
+    forward(50)
+    left(120)    
--- hogehoge/test.py
+++ hogehoge/test.py
@@ -3,13 +3,22 @@ shape("turtle")
 n = int(input('ピラミッドの段数を入力してください。'))
 if n == 1:
  for n in range(3):
+    forward(100)     
     left(120)
-    forward(100)
 elif n == 2:
-    left(120)
+ for n in range(3):
     forward(50)
     left(120)
+    continue
+right(120)
+for n in range(3):
     forward(50)
     left(120)
-    forward(100)
+    continue
+forward(50)
+left(120)
+forward(50)
+for n in range(3):
+    forward(50)
+    left(120)
 done()
