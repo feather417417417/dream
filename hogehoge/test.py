74c28daf7bffd84a691f88ae4e25bbf4c17fd75b
feather417417417 <109892530+feather417417417@users.noreply.github.com>
2023-02-12 23:54:50 +0900

    ピラミッド作成中 その4 ifを超えて3段目、4段目まで作成完了と少しブラッシュアップ実施

--- hogehoge/test.py
+++ hogehoge/test.py
@@ -1,44 +1,170 @@
 from turtle import *
 shape("turtle")
 n = int(input('ピラミッドの段数を入力してください。'))
-if n == 1:
+if  n == 1:
  for n in range(3):
     forward(100)     
     left(120)
 
-if n == 2:
+elif n == 2:
  for n in range(3):
     forward(50)
     left(120)
     continue
-right(120)
-for n in range(3):
+ right(120)
+ for n in range(4):
     forward(50)
     left(120)
     continue
-forward(50)
-left(120)
-forward(50)
-for n in range(3):
+ forward(50)
+ for n in range(3):
     forward(50)
     left(120)
-done()
 
-if n == 3:
+elif n == 3:
  for n in range(3):
    forward(50)
    left(120)
    continue
-right(120)
-for n in range(3):
+ right(120)
+ for n in range(4):
     forward(50)
     left(120)
     continue
-forward(50)
-left(120)
-forward(50)
-for n in range(3):
+ forward(50)
+ for n in range(3):
     forward(50)
     left(120)  
-    forward(150)      
+    continue
+ right(180)
+ forward(50)
+ right(60)    
+ for n in range(3):
+    left(120) 
+    forward(50) 
+    continue
+ for n in range(2):
+    left(120) 
+    forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)      
+
+elif n == 4:
+ for n in range(3):
+   forward(50)
+   left(120)
+   continue
+ right(120)
+ for n in range(4):
+    forward(50)
+    left(120)
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ right(180)
+ forward(50)
+ right(60)    
+ for n in range(5):
+    left(120) 
+    forward(50) 
+    continue  
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)   
+    continue
+ right(180)
+ forward(100)
+ left(60)
+ for n in range(4):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120) 
+    continue 
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)    
+    continue
+
+elif n == 5:
+ for n in range(3):
+   forward(50)
+   left(120)
+   continue
+ right(120)
+ for n in range(4):
+    forward(50)
+    left(120)
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ right(180)
+ forward(50)
+ right(60)    
+ for n in range(4):
+    left(120) 
+    forward(50) 
+    continue
+ left(120)
+ forward(50)  
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)   
+    continue
+ right(180)
+ forward(100)
+ left(60)
+ for n in range(4):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120) 
+    continue 
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)    
+    continue
+ right(180)
+ forward(150)
+ 
 done()
