4707c0b27843023be0578ac67f7d9f88b7fe9ef8
feather417417417 <109892530+feather417417417@users.noreply.github.com>
2023-02-19 21:37:08 +0900

    関数お試し作成

--- hogehoge/memo.py
+++ hogehoge/memo.py
@@ -1,17 +1,203 @@
-#3段目増やしたいけどエラーにつきメモ
+from turtle import *
+shape("turtle")
+n = int(input('ピラミッドの段数を入力してください。'))
+
+#1段目
+if  n == 1:
+ for n in range(3):
+    forward(100)     
+    left(120)
+
+#2段目
+elif n == 2:
+ for n in range(3):
+    forward(50)
+    left(120)
+    continue
+ right(120)
+ for n in range(4):
+    forward(50)
+    left(120)
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)
+
+#3段目
 elif n == 3:
- for n in range(3):E
+ for n in range(3):
+   forward(50)
+   left(120)
+   continue
+ right(120)
+ for n in range(4):
     forward(50)
     left(120)
     continue
-right(120)
-for n in range(3):
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ right(180)
+ forward(50)
+ right(60)    
+ for n in range(3):
+    left(120) 
+    forward(50) 
+    continue
+ for n in range(2):
+    left(120) 
+    forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)      
+    
+#4段目    
+elif n == 4:
+ for n in range(3):
+   forward(50)
+   left(120)
+   continue
+ right(120)
+ for n in range(4):
     forward(50)
     left(120)
     continue
-forward(50)
-left(120)
-forward(50)
-for n in range(3):
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ right(180)
+ forward(50)
+ right(60)    
+ for n in range(5):
+    left(120) 
+    forward(50) 
+    continue  
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)   
+    continue
+ right(180)
+ forward(100)
+ left(60)
+ for n in range(4):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120) 
+    continue 
+ forward(50)
+ for n in range(3):
     forward(50)
     left(120)    
+    continue
+
+#5段目
+elif n == 5:
+ for n in range(3):
+   forward(50)
+   left(120)
+   continue
+ right(120)
+ for n in range(4):
+    forward(50)
+    left(120)
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ right(180)
+ forward(50)
+ right(60)    
+ for n in range(4):
+    left(120) 
+    forward(50) 
+    continue
+ left(120)
+ forward(50)  
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50) 
+ for n in range(3):
+    forward(50)
+    left(120)   
+    continue
+ right(180)
+ forward(100)
+ left(60)
+ for n in range(4):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)  
+    continue
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120) 
+    continue 
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)    
+    continue
+ right(180)
+ forward(150)
+ left(60)
+ for n in range(4):
+    forward(50)
+    left(120)  
+    continue 
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)   
+    continue 
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)
+    continue 
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)
+    continue 
+ forward(50)
+ for n in range(3):
+    forward(50)
+    left(120)
+    continue 
+ right(180)
+ forward(200)
+  
+done()
---
+++ hogehoge/test3.py
@@ -0,0 +1,8 @@
+def test(n):
+    if n == 3:
+        print("成功")
+    else:
+       print("微妙")
+
+n = int(input('ピラミッドの段数を入力してください。'))
+test(n)
