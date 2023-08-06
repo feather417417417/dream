b9b2728d9106af588a1b9f1922be347fa16a2423
feather417417417 <109892530+feather417417417@users.noreply.github.com>
2023-08-06 19:50:41 +0900

    亀とdesk

--- hogehoge/turtle1.py
+++ hogehoge/turtle1.py
@@ -1,8 +1,15 @@
 from turtle import *
+import sys
 shape("turtle")
 col = ["orange","gold","pink","blue","red"]
 for i in range(5):
     color(col[i])
     forward(200)
     left(144)
+if i ==5:
+    sys.exit(0)
 done()
+quit()
+
+
+
